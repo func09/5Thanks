794ccc93acf78cfcf91284d1380539c11beb7155
func09 <mitsuru.haga@gmail.com>
2011-09-08 02:56:56 +0900

    Add Default Host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,6 +58,10 @@ Kanshabox::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   
+  config.action_mailer.default_url_options = { 
+    :host => 'fivethanks.heroku.com'
+  }
+  
   ActionMailer::Base.smtp_settings = {
     :address        => "smtp.sendgrid.net",
     :port           => "25",
